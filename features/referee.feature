4284bd354fc6ef70177f5c82549e33beb73c725f
Jackson Zhang <zbcjackson@gmail.com>
2014-04-15 15:42:49 +0800

    Add referee features

---
+++ features/referee.feature
@@ -0,0 +1,5 @@
+Feature: Referee feature
+
+  Scenario: As a referee I can get current score
+    Given The tennis game starts
+    Then I see the score "Love All"
---
+++ features/step_definitions/referee_steps.rb
@@ -0,0 +1,7 @@
+Given(/^The tennis game starts$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see the score "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
