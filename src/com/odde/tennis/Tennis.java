38c23bcbddfce8f246f84ab329e79ce9fba20c17
Jackson Zhang <zbcjackson@gmail.com>
2014-04-17 10:29:27 +0800

    Fifteen Love

--- src/com/odde/tennis/Tennis.java
+++ src/com/odde/tennis/Tennis.java
@@ -1,7 +1,15 @@
 package com.odde.tennis;
 
 public class Tennis {
+    private int scoreOfPlayer1;
+
     public String getScore() {
+        if (scoreOfPlayer1 == 15)
+            return "Fifteen Love";
         return "Love All";
+    }
+
+    public void player1Score() {
+        scoreOfPlayer1 = 15;
     }
 }
--- test/TennisTest.java
+++ test/TennisTest.java
@@ -4,9 +4,17 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TennisTest {
+    private Tennis game = new Tennis();
+
     @Test
     public void love_all(){
-        Tennis game = new Tennis();
         assertEquals("Love All", game.getScore());
+    }
+
+    @Test
+    public void fifteen_love_after_player1_scores() throws Exception {
+        game.player1Score();
+        assertEquals("Fifteen Love", game.getScore());
+
     }
 }
