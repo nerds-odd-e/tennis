71cf62b88110f0981ff2aeddd595ab66f840bc25
Jackson Zhang <zbcjackson@gmail.com>
2014-04-17 10:19:34 +0800

    Integrate with Tennis class

--- src/com/odde/tennis/Referee.java
+++ src/com/odde/tennis/Referee.java
@@ -2,8 +2,11 @@ package com.odde.tennis;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Referee extends Activity {
+    private Tennis game;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +14,9 @@ public class Referee extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        this.game = new Tennis();
+        TextView score = (TextView)findViewById(R.id.score);
+        score.setText(game.getScore());
     }
 }
