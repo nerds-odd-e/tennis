2c4a7bfc641e8a670948f4af5c3c409b2a994c1c
Jackson Zhang <zbcjackson@gmail.com>
2014-04-17 10:43:10 +0800

    Integrate player1 scores button

--- src/com/odde/tennis/Referee.java
+++ src/com/odde/tennis/Referee.java
@@ -2,21 +2,24 @@ package com.odde.tennis;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Referee extends Activity {
-    private Tennis game;
+    private Tennis game = new Tennis();
+    private TextView score;
 
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        this.game = new Tennis();
-        TextView score = (TextView)findViewById(R.id.score);
+        score = (TextView)findViewById(R.id.score);
+        score.setText(game.getScore());
+    }
+
+    public void player1Score(View view){
+        game.player1Score();
         score.setText(game.getScore());
     }
 }
